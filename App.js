3630bbe64134b47d7f60bef2015a13dbcefd6754
Kalyani Jamunkar <kalyani@gatha.co.in>
2019-12-04 15:11:19 +0530

    tinder swipe

---
+++ App.js
@@ -0,0 +1,183 @@
+import React, { Component } from 'react'
+import Swiper from 'react-native-deck-swiper'
+import { ImageBackground, Button, StyleSheet, Text, View } from 'react-native'
+
+// demo purposes only
+const data = [{name: 'kalyani'}, {name: 'john'}, {name: 'david'}]
+
+export default class Exemple extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: data,
+      swipedAllCards: false,
+      swipeDirection: '',
+      cardIndex: 0
+    }
+  }
+
+  renderCard = (card, index) => {
+
+    return (
+      <View style={styles.card}>
+      <ImageBackground source={require('./image.jpg')} style={{flex: 1, margin: 20, width: 300}}  blurRadius={5}>
+      <Text style={styles.text}>{card.name}</Text>
+
+      </ImageBackground>
+
+
+      </View>
+    )
+  };
+
+  onSwiped = (type) => {
+    console.log(`on swiped ${type}`)
+  }
+
+  onSwipedAllCards = () => {
+    this.setState({
+      swipedAllCards: true
+    })
+  };
+
+  swipeLeft = () => {
+    this.swiper.swipeLeft()
+  };
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <Swiper
+          ref={swiper => {
+            this.swiper = swiper
+          }}
+          onSwiped={() => this.onSwiped('general')}
+          onSwipedLeft={() => this.onSwiped('left')}
+          onSwipedRight={() => this.onSwiped('right')}
+          onSwipedTop={() => this.onSwiped('top')}
+          onSwipedBottom={() => this.onSwiped('bottom')}
+          onTapCard={this.swipeLeft}
+          cards={this.state.cards}
+          cardIndex={this.state.cardIndex}
+          cardVerticalMargin={80}
+          renderCard={this.renderCard}
+          onSwipedAll={this.onSwipedAllCards}
+          stackSize={3}
+          stackSeparation={15}
+          backgroundColor={'#ffffff'}
+          overlayLabels={{
+            bottom: {
+              title: 'BLEAH',
+              style: {
+                label: {
+                  backgroundColor: 'black',
+                  borderColor: 'black',
+                  color: 'white',
+                  borderWidth: 1
+                },
+                wrapper: {
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center'
+                }
+              }
+            },
+            left: {
+              title: 'NOPE',
+              style: {
+                label: {
+                  backgroundColor: 'black',
+                  borderColor: 'black',
+                  color: 'white',
+                  borderWidth: 1
+                },
+                wrapper: {
+                  flexDirection: 'column',
+                  alignItems: 'flex-end',
+                  justifyContent: 'flex-start',
+                  marginTop: 30,
+                  marginLeft: -30,
+                  backgroundColor: '#90ee90',
+                  height: 450
+                }
+              }
+            },
+            right: {
+              title: 'LIKE',
+              style: {
+                label: {
+                  backgroundColor: 'black',
+                  borderColor: 'black',
+                  color: 'white',
+                  borderWidth: 1
+                },
+                wrapper: {
+                  flexDirection: 'column',
+                  alignItems: 'flex-start',
+                  justifyContent: 'flex-start',
+                  marginTop: 30,
+                  marginLeft: 30,
+                   backgroundColor: '#ffb6c1',
+                   height: 450
+                }
+              }
+            },
+            top: {
+              title: 'SUPER LIKE',
+              style: {
+                label: {
+                  backgroundColor: 'black',
+                  borderColor: 'black',
+                  color: 'white',
+                  borderWidth: 1
+                },
+                wrapper: {
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center'
+                }
+              }
+            }
+          }}
+          animateOverlayLabelsOpacity
+          animateCardOpacity
+          swipeBackCard
+        >
+          <Button onPress={() => this.swiper.swipeBack()} title='Swipe Back' />
+          <View style={{flexDirection: 'row', alignItems: 'center', alignSelf: 'center'}}>
+             <Button onPress={() => this.swiper.swipeLeft()} title='Skip' />
+
+              <Button onPress={() => this.swiper.swipeRight()} title='Save' />
+              </View>
+        </Swiper>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF'
+  },
+  card: {
+    flex: 1,
+    borderRadius: 16,
+    borderWidth: 2,
+    borderColor: '#E8E8E8',
+    justifyContent: 'center',
+    backgroundColor: 'white'
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 50,
+    backgroundColor: 'transparent',
+    color: '#ffffff'
+  },
+  done: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: 'white',
+    backgroundColor: 'transparent'
+  }
+})
